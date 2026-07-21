bca530c076920b7af5731f1438f19843823dedd6
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-07-21 17:44:32 -0300

    bug fix camara y rotacion

--- PROYECTO JUEGO MALE/escenas/camaras_manager.gd
+++ PROYECTO JUEGO MALE/escenas/camaras_manager.gd
@@ -3,13 +3,21 @@ extends Node3D
 #hago un manager centralizar donde se van a cambiar las camaras
 #para cambiar entre camara jugador y camara espectando
 
+@export var body : Player
+@export var camara_jugador : Camera3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if not body.is_multiplayer_authority():
+		return
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camara_jugador.current = true
+	
 
 
 func _input(event: InputEvent) -> void:
+	if not body.is_multiplayer_authority():
+		return
 	if Input.is_action_just_pressed("escape"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
